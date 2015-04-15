9b5390dedd698c6ef070e8baa809af245e580521
maikelsfresne <maikelsvivaundertaker@hotmail.com>
2015-04-15 18:09:07 +0200

    Update clase2.java

--- presentacionED/src/presentacionED/clase2.java
+++ presentacionED/src/presentacionED/clase2.java
@@ -2,6 +2,6 @@ package presentacionED;
 
 public class clase2 {
 	public static void main(String[] args) {
-		System.out.println("Mensajeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
+		System.out.println("Mensaje");
 	}
 }
