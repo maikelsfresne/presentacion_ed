229efbadf54341f95747ca21af511ffa86d305a2
maikelsfresne <maikelsvivaundertaker@hotmail.com>
2015-04-15 17:37:35 +0200

    Update clase1.java

--- presentacionED/src/presentacionED/clase1.java
+++ presentacionED/src/presentacionED/clase1.java
@@ -2,6 +2,6 @@ package presentacionED;
 
 public class clase1 {
 	public static void main(String[] args) {
-		System.out.println("Hola, acabo de modificar esta línea de código para probar el sincronizador");
+		System.out.println("Hola");
 	}
 }
