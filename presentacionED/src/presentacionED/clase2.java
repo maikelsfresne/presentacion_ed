4e1f28ad4bbb9403a73a276045ab16a32fb2ebff
cesinolu <cesinolu@gmail.com>
2015-04-16 12:27:36 +0200

    3.6

--- presentacionED/src/presentacionED/clase2.java
+++ presentacionED/src/presentacionED/clase2.java
@@ -2,6 +2,6 @@ package presentacionED;
 
 public class clase2 {
 	public static void main(String[] args) {
-		System.out.println("Mensaje de pruebas 3.5");
+		System.out.println("Mensaje de pruebas 3.6");
 	}
 }
