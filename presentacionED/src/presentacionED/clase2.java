c88ebba88b59cb5ca155d068a5669b560c1317be
cesinolu <cesinolu@gmail.com>
2015-04-16 12:03:31 +0200

    Ahora 2.0

--- presentacionED/src/presentacionED/clase2.java
+++ presentacionED/src/presentacionED/clase2.java
@@ -2,6 +2,6 @@ package presentacionED;
 
 public class clase2 {
 	public static void main(String[] args) {
-		System.out.println("Mensaje de pruebas");
+		System.out.println("Mensaje de pruebas 2.0");
 	}
 }
